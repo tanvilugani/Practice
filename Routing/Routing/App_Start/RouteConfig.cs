14540741744c2ffbfc4d0d167c2b23dbe02bf3f4
Tanvi Lugani <luganitanvi@yahoo.co.in>
2016-11-24 09:00:48 +0530

    1. Added comment for clarity.

--- Routing/Routing/App_Start/RouteConfig.cs
+++ Routing/Routing/App_Start/RouteConfig.cs
@@ -13,10 +13,12 @@ namespace Routing
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Mandatory to give a route with name Default
+
             //Test Scenario 1 : Hardcode some value as it is here : Home
             routes.MapRoute(
                 name: "Default",
-                url: "Home/{action}/{id}",
+                url: "HomeHome/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
                 //Mandatory to give a controller name in Default otherwise no controller specified error.
             );
@@ -25,22 +27,19 @@ namespace Routing
             //Similar routes but the one that matches first is used. Uncomment and swap with TC 1 to verify results.
             //routes.MapRoute(
             //    name: "Default1",
-            //    url: "Home/Index/{id}",
+            //    url: "HomeHome/Index/{id}",
             //    defaults: new { controller = "Home", action = "Print", id = UrlParameter.Optional }
             //    //Mandatory to give a controller name in Default otherwise no controller specified error.
             //);
 
-            
-
             //Test Scenario 2  : Change order of placeholders in URL.
             //This is similar to the original pattern. The keys action and controller stands for MVC action and controllers.
             routes.MapRoute(
-                name: "Default2",
+                name: "Route2",
                 url: "{action}/{controller}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
                 //Mandatory to give a controller name in Default otherwise no controller specified error.
             );
-
         }
     }
 }
