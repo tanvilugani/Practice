1458631349745f113cfc3a3cd53c41d345f04524
Tanvi Lugani <luganitanvi@yahoo.co.in>
2016-12-31 07:54:47 +0530

    Yield keyword using power of 2 example.

---
+++ YieldKeyword/YieldKeyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace YieldKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display powers of 2 up to the exponent of 8:
+            foreach (int i in Power(2, 8))
+            {
+                Console.Write("{0} ", i);                
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Yield keyword is used when a function, operator or accessor is an iterator. 
+        /// It returns value for each element and retains the state of the function.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="exponent"></param>
+        /// <returns></returns>
+        public static System.Collections.Generic.IEnumerable<int> Power(int number, int exponent)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exponent; i++)
+            {
+                result = result * number;
+                yield return result;
+            }
+        }
+    }
+}
