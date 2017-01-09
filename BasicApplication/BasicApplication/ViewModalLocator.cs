5157a34a3baf2bec300c9cac7acdc485f3323784
tanvi.lugani <luganitanvi@yahoo.co.in>
2017-01-09 22:47:55 +0530

    Implemented Add functionality using View Model approach.

---
+++ BasicApplication/BasicApplication/ViewModalLocator.cs
@@ -0,0 +1,26 @@
+﻿using BasicApplication.ViewModel;
+
+namespace BasicApplication
+{
+    class ViewModalLocator
+    {
+        private static EmployeesViewModel _employeesViewModel = new EmployeesViewModel();
+        private static AddEmployeeViewModel _addEmployeeViewModel = new AddEmployeeViewModel();
+
+        public static EmployeesViewModel EmployeesViewModel
+        {
+            get
+            {
+                return _employeesViewModel;
+            }
+        }
+
+        public static AddEmployeeViewModel AddEmployeeViewModel
+        {
+            get
+            {
+                return _addEmployeeViewModel;
+            }
+        }
+    }
+}
