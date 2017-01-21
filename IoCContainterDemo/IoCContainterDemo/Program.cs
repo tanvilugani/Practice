91b0e3b3789c7c29f52b70546a6a35ab9201c259
Tanvi Lugani <luganitanvi@yahoo.co.in>
2017-01-21 21:23:13 +0530

    Updated solution to use an IoC Container

--- IoCContainterDemo/IoCContainterDemo/Program.cs
+++ IoCContainterDemo/IoCContainterDemo/Program.cs
@@ -6,16 +6,21 @@ namespace IoCContainterDemo
     {
         public static void Main(string[] args)
         {
-            ICreditCard masterCard = new MasterCard();
-            ICreditCard visaCard = new VisaCard();
+            //ICreditCard masterCard = new MasterCard();
+            //ICreditCard visaCard = new VisaCard();
+            //var anotherShopper = new Shopper(visaCard);
+            //anotherShopper.Charge();
 
-            var shopper = new Shopper(masterCard);
+            //var shopper = new Shopper(new Resolver().ResolveCreditCard());
+
+            var resolver = new Resolver();
+
+            resolver.Register<Shopper, Shopper>();
+            resolver.Register<ICreditCard, MasterCard>();
+
+            var shopper = resolver.Resolve<Shopper>();
 
             shopper.Charge();
-
-            var anotherShopper = new Shopper(visaCard);
-
-            anotherShopper.Charge();
 
             Console.ReadKey();            
         }
---
+++ IoCContainterDemo/IoCContainterDemo/Resolver.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoCContainterDemo
+{
+    class Resolver
+    {
+        /// <summary>
+        /// Will include business logic for deciding if a master credit card or Visa Credit card.
+        /// </summary>
+        /// <returns></returns>
+        //public ICreditCard ResolveCreditCard()
+        //{
+        //    if (new Random().Next(2) == 1)
+        //        return new MasterCard();
+
+        //    return new VisaCard();
+        //}
+
+        private Dictionary<Type, Type> _dependencyMap = new Dictionary<Type, Type>();
+
+        public T Resolve<T>()
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        private object Resolve(Type typeToResolve)
+        {
+            Type resolvedType = null;
+
+            try
+            {
+                resolvedType = _dependencyMap[typeToResolve];
+            }
+            catch (Exception)
+            {
+                throw new Exception("Could not resolve type");
+                throw;
+            }
+
+            var firstConstructor = resolvedType.GetConstructors().First();
+            var constructorParameters = firstConstructor.GetParameters();
+
+            if (constructorParameters.Count() == 0)
+                return Activator.CreateInstance(resolvedType);
+
+            var parameters = new List<object>();
+
+            foreach (var parametersToResolve in constructorParameters)
+            {
+                parameters.Add(Resolve(parametersToResolve.ParameterType));
+            }
+
+            return firstConstructor.Invoke(parameters.ToArray());
+        }
+
+        internal void Register<T1, T2>()
+        {
+            _dependencyMap.Add(typeof(T1), typeof(T2));
+        }
+    }
+}
